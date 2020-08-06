1263ef3ee7ecdfc518b3fc435ec371c6de077b09
Ajay Seemar <rainbowmaan@outlook.com>
2020-08-05 20:37:50 -0700

    Initialize gql and create a temporary query to test in the gql playground

---
+++ gql/resolvers/hello_resolver.py
@@ -0,0 +1,5 @@
+from gql.resolvers.root_resolver import query
+
+@query.field("hello")
+def resolve_hello(_, info):
+    return "Sa"
---
+++ gql/resolvers/root_resolver.py
@@ -0,0 +1,6 @@
+from ariadne import QueryType
+
+query = QueryType()
+
+# import resolvers
+from gql.resolvers.hello_resolver import resolve_hello

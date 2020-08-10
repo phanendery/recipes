4bc34ff45fa999fd6795a3be77c4282f8114ad40
phanendery <phanendery@gmail.com>
2020-08-10 15:48:51 -0700

    stuff happened

--- api/gql/resolvers/users_resolver.py
+++ api/gql/resolvers/users_resolver.py
@@ -1,10 +1,13 @@
 from api.gql.resolvers.root_resolver import query
+from api.server import db
 
 @query.field("user")
 def resolve_user(_, info):
     return {
-        "id": 1,
-        "first_name": "Ivana",
-        "last_name": "Tinkle",
-        "email": "iTinkle69@email.com"
+
+        
+        # "id": 1,
+        # "first_name": "Ivana",
+        # "last_name": "Tinkle",
+        # "email": "iTinkle69@email.com"
     }
